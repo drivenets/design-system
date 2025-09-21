a8a2559c484c5d99c23d137592e9601a863cd7c5
Rina Roi <rr155p@intl.att.com>
2025-09-21 10:13:06 +0300

    AR-5760: move devices table to use ds-table with known bugs

--- src/lib/components/ds-table/index.ts
+++ src/lib/components/ds-table/index.ts
@@ -1,2 +1,3 @@
 export { default as DsTable } from './ds-table';
 export * from './ds-table.types';
+export type { SecondaryRowAction, RowAction } from './components/ds-table-cell/ds-table-cell.types';
