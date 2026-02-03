97f7bc695655549cf857d099e2419af1a25227f9
Ihor Romanchuk <ihor.romanchuk@drivenets.com>
2026-02-03 21:33:01 +0100

    fix(design-system): use mockdate in storybook files instead of vi [AR-46279]

--- packages/design-system/src/components/ds-date-input/ds-date-input.stories.tsx
+++ packages/design-system/src/components/ds-date-input/ds-date-input.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import { useState } from 'react';
 import { expect, screen, userEvent, waitFor, within } from 'storybook/test';
-import { vi } from 'vitest';
+import MockDate from 'mockdate';
 import DsDateInput from './ds-date-input';
 import type { DsDateInputProps } from './ds-date-input.types';
 import styles from './ds-date-input.stories.module.scss';
@@ -17,14 +17,10 @@ const meta: Meta<typeof DsDateInput> = {
 		layout: 'centered',
 	},
 	beforeEach: () => {
-		vi.useFakeTimers({
-			// needed,
-			shouldAdvanceTime: true,
-		});
-		vi.setSystemTime(MOCK_DATE);
+		MockDate.set(MOCK_DATE);
 
 		return () => {
-			vi.useRealTimers();
+			MockDate.reset();
 		};
 	},
 };
