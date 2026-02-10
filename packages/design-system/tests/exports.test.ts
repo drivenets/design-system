b2c2970aedab9d754945e2271d37b1558ef3ca5c
StyleShit <32631382+StyleShit@users.noreply.github.com>
2026-02-10 12:02:23 +0200

    wip

--- packages/design-system/tests/exports.test.ts
+++ packages/design-system/tests/exports.test.ts
@@ -2,26 +2,26 @@ import fs from 'node:fs';
 import { describe, expect, it } from 'vitest';
 
 describe('Design System exports', () => {
+	const expectedLines = fs
+		.readdirSync('./src/components')
+		.toSorted()
+		.map((component) => {
+			return `export * from './components/${component}';`;
+		});
+
+	const actualContent = fs.readFileSync('./src/index.ts', 'utf-8');
+	const actualLines = actualContent.split('\n').filter((line) => line.trim().length > 0);
+
+	const tests = expectedLines.map((expectedLine, index) => {
+		return [actualLines[index], expectedLine];
+	});
+
 	it('should export all components', () => {
-		const expectedLines = fs
-			.readdirSync('./src/components')
-			.toSorted()
-			.map((component) => {
-				return `export * from './components/${component}';`;
-			});
-
-		const actualContent = fs.readFileSync('./src/index.ts', 'utf-8');
-		const actualLines = actualContent.split('\n').filter((line) => line.trim().length > 0);
-
 		expect(actualLines.length).toBe(expectedLines.length);
+	});
 
-		const tests = expectedLines.map((expectedLine, index) => {
-			return [actualLines[index], expectedLine];
-		});
-
-		// split expected by line and assert that each line is exported
-		it.each(tests)('"%s" is exported from the package', (actual, expected) => {
-			expect(actual).toBe(expected);
-		});
+	// split expected by line and assert that each line is exported
+	it.each(tests)('"%s" is exported from the package', (actual, expected) => {
+		expect(actual).toBe(expected);
 	});
 });
