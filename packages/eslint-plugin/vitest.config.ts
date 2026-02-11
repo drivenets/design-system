672d5068c5e43c974bd82538581248c930422b4b
Ihor Romanchuk <ihor.romanchuk@drivenets.com>
2026-02-11 17:20:52 +0100

    build(test): set code coverage threshold [AR-47601]

--- packages/eslint-plugin/vitest.config.ts
+++ packages/eslint-plugin/vitest.config.ts
@@ -3,5 +3,16 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	test: {
 		setupFiles: ['./vitest.setup.ts'],
+		coverage: {
+			thresholds: {
+				lines: 90,
+				functions: 90,
+				branches: 90,
+				statements: 90,
+			},
+			watermarks: {
+				statements: [80, 90],
+			},
+		},
 	},
 });
