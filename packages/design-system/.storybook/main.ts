9f3ae7e93d3f3c59e70727dfbc0065cb300bfc90
StyleShit <32631382+StyleShit@users.noreply.github.com>
2026-03-19 15:01:54 +0200

    wip

--- packages/design-system/.storybook/main.ts
+++ packages/design-system/.storybook/main.ts
@@ -1,5 +1,4 @@
 import type { StorybookConfig } from '@storybook/react-vite';
-import { withoutVitePlugins } from '@storybook/builder-vite';
 import { vitePluginDesignSystem } from '@drivenets/vite-plugin-design-system';
 import { reactCompilerRolldownPlugin } from '../rolldown/react-compiler-rolldown-plugin.ts';
 
@@ -7,16 +6,13 @@ const config: StorybookConfig = {
 	stories: ['../src/**/!(*.docs).mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 	addons: ['@storybook/addon-vitest', '@storybook/addon-a11y', '@storybook/addon-docs'],
 	framework: '@storybook/react-vite',
-	viteFinal: async (viteConfig) => {
+	viteFinal: (viteConfig) => {
 		if (!Array.isArray(viteConfig.plugins)) {
 			viteConfig.plugins = [];
 		}
 
 		viteConfig.plugins.push(vitePluginDesignSystem());
 		viteConfig.plugins.push(reactCompilerRolldownPlugin());
-
-		// Storybook build doesn't need to generate d.ts files.
-		viteConfig.plugins = await withoutVitePlugins(viteConfig.plugins, ['vite:dts']);
 
 		return viteConfig;
 	},
