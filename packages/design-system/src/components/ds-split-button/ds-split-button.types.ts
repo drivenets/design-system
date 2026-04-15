541034ee3acd2c93b7989c539246862612dff5b9
Ihor Romanchuk <ihor.romanchuk@drivenets.com>
2026-04-15 12:32:00 +0200

    Move DistributiveOmit into type-utils.ts

--- packages/design-system/src/components/ds-split-button/ds-split-button.types.ts
+++ packages/design-system/src/components/ds-split-button/ds-split-button.types.ts
@@ -1,13 +1,13 @@
 import type { CSSProperties, Ref } from 'react';
 import type { DsButtonV3Props } from '../ds-button-v3';
 import type { DsSelectProps } from '../ds-select';
+import type { DistributiveOmit } from '../../utils/type-utils';
 
 export const splitButtonSizes = ['medium', 'small'] as const;
 export type SplitButtonSize = (typeof splitButtonSizes)[number];
 
 type ButtonSlotProps = Omit<DsButtonV3Props, 'variant' | 'color' | 'size'>;
 
-type DistributiveOmit<T, K extends PropertyKey> = T extends unknown ? Omit<T, K> : never;
 type SelectSlotProps = DistributiveOmit<DsSelectProps, 'size'>;
 
 export interface DsSplitButtonSlotProps {
