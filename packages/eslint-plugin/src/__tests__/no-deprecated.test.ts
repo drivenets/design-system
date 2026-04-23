e14f727cc970b4f1c591b2140ed302a9f7e903fb
Micky <michal.murawski@drivenets.com>
2026-04-23 12:13:12 +0200

    refactor(design-system)!: remove DsDateInput component [AR-53409] (#400)

--- packages/eslint-plugin/src/__tests__/no-deprecated.test.ts
+++ packages/eslint-plugin/src/__tests__/no-deprecated.test.ts
@@ -1,5 +1,5 @@
 import { RuleTester } from 'eslint';
-import plugin from '../index';
+import { expect, it } from 'vitest';
 
 const ruleTester = new RuleTester({
 	languageOptions: {
@@ -11,40 +11,27 @@ const ruleTester = new RuleTester({
 	},
 });
 
-ruleTester.run('no-deprecated-ds-date-input', plugin.rules['no-deprecated-ds-date-input'], {
-	valid: ['<DsDatePicker />', '<DsDateRangePicker />'],
-
-	invalid: [
-		{
-			code: '<DsDateInput />',
-			errors: [
-				{
-					message: `DsDateInput is deprecated. Use DsDatePicker or DsDateRangePicker instead.`,
-					line: 1,
-					endLine: 1,
-					column: 2,
-					endColumn: 13,
-				},
-			],
-		},
-	],
+it('placeholder: RuleTester instantiates', () => {
+	expect(ruleTester).toBeInstanceOf(RuleTester);
 });
 
-ruleTester.run('no-deprecated-ds-popover-legacy', plugin.rules['no-deprecated-ds-popover-legacy'], {
-	valid: ['<DsPopover.Root />', '<DsPopover.Trigger />', '<DsPopover.Panel />'],
+// Example test for the future reference
 
-	invalid: [
-		{
-			code: '<DsPopover trigger={<button />}>Content</DsPopover>',
-			errors: [
-				{
-					message: `The DsPopover callable form (with a 'trigger' prop) is deprecated. Use the compound API: DsPopover.Root with DsPopover.Trigger and DsPopover.Panel.`,
-					line: 1,
-					endLine: 1,
-					column: 2,
-					endColumn: 11,
-				},
-			],
-		},
-	],
-});
+// ruleTester.run('no-deprecated-ds-date-input', plugin.rules['no-deprecated-ds-date-input'], {
+// 	valid: ['<DsDatePicker />', '<DsDateRangePicker />'],
+
+// 	invalid: [
+// 		{
+// 			code: '<DsDateInput />',
+// 			errors: [
+// 				{
+// 					message: `DsDateInput is deprecated. Use DsDatePicker or DsDateRangePicker instead.`,
+// 					line: 1,
+// 					endLine: 1,
+// 					column: 2,
+// 					endColumn: 13,
+// 				},
+// 			],
+// 		},
+// 	],
+// });
