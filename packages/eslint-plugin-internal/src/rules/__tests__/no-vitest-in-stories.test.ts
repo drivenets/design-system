d45701cf4475f54c23573be335d6203bfcf19f81
StyleShit <32631382+StyleShit@users.noreply.github.com>
2026-06-10 12:06:31 +0300

    chore(eslint-plugin-internal): require error position in tests [AR-53188]

--- packages/eslint-plugin-internal/src/rules/__tests__/no-vitest-in-stories.test.ts
+++ packages/eslint-plugin-internal/src/rules/__tests__/no-vitest-in-stories.test.ts
@@ -14,23 +14,23 @@ ruleTester.run('no-vitest-in-stories', noVitestInStories, {
 	invalid: [
 		{
 			code: "import { vi } from 'vitest';",
-			errors: [{ messageId: 'noVitestInStories' }],
+			errors: [{ messageId: 'noVitestInStories', line: 1, column: 20, endLine: 1, endColumn: 28 }],
 		},
 		{
 			code: "import { expect } from 'vitest';",
-			errors: [{ messageId: 'noVitestInStories' }],
+			errors: [{ messageId: 'noVitestInStories', line: 1, column: 24, endLine: 1, endColumn: 32 }],
 		},
 		{
 			code: "import { vi } from 'vitest/utils';",
-			errors: [{ messageId: 'noVitestInStories' }],
+			errors: [{ messageId: 'noVitestInStories', line: 1, column: 20, endLine: 1, endColumn: 34 }],
 		},
 		{
 			code: "import { Mock } from '@vitest/spy';",
-			errors: [{ messageId: 'noVitestInStories' }],
+			errors: [{ messageId: 'noVitestInStories', line: 1, column: 22, endLine: 1, endColumn: 35 }],
 		},
 		{
 			code: "import { page } from 'vitest/browser';",
-			errors: [{ messageId: 'noVitestInStories' }],
+			errors: [{ messageId: 'noVitestInStories', line: 1, column: 22, endLine: 1, endColumn: 38 }],
 		},
 	],
 });
