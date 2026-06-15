2ad6f5e80fcb00329de106978392d7f78a7ab8d0
Evyatar Daud <32631382+StyleShit@users.noreply.github.com>
2026-06-15 08:21:04 +0300

    ci(pr-name-linter): validate breaking changes commits [AR-57660] (#547)

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,10 +1,13 @@
 import { RuleConfigSeverity, type UserConfig } from '@commitlint/types';
 import designSystem from '@drivenets/commitlint-plugin-internal';
 
+const hasBreakingChanges = process.env.HAS_BREAKING_CHANGES === 'true';
+
 const config: UserConfig = {
 	extends: ['@commitlint/config-conventional'],
 	plugins: [designSystem],
 	rules: {
+		'design-system/exclamation-mark': [RuleConfigSeverity.Error, hasBreakingChanges ? 'always' : 'never'],
 		'design-system/require-jira-ticket': [RuleConfigSeverity.Error, 'always', { prefix: 'AR' }],
 	},
 };
